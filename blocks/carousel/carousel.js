616fba2379084ca84ac447da5fd498938b559c68
Ashwin Jethawa <Ashwin.Jethawa@teknopoint.local>
2025-03-20 14:52:09 +0530

    Inital Changes

---
+++ blocks/carousel/carousel.js
@@ -0,0 +1,51 @@
+import SwiperText from "../carousel/swiper-bundle.min.js"
+
+export default function decorate(block) {
+    // console.log(block);
+    block.classList.add("swiper")
+
+    const swiperWrapper = document.createElement('div');
+    swiperWrapper.classList.add('swiper-wrapper');
+    Array.from(block.children).forEach((row) => {
+        row.classList.add("swiper-slide")
+        swiperWrapper.appendChild(row)
+    })
+    block.append(swiperWrapper);
+
+    const paginationWrapper = document.createElement("div")
+    paginationWrapper.classList.add("swiper-pagination");
+    block.appendChild(paginationWrapper);
+
+    // const LeftArrow = document.createElement("div");
+    // LeftArrow.classList.add("swiper-button-prev");
+    // block.appendChild(LeftArrow);
+
+    // const RightArrow = document.createElement("div")
+    // RightArrow.classList.add("swiper-button-next");
+    // block.appendChild(RightArrow);
+
+
+    SwiperText(block, {
+        // Optional parameters
+        // direction: 'vertical',
+        loop: true,
+        autoplay: true,
+
+        // If we need pagination
+        pagination: {
+            el: paginationWrapper,
+            clickable: true,
+        },
+
+        // // Navigation arrows
+        // navigation: {
+        //     nextEl: RightArrow,
+        //     prevEl: LeftArrow,
+        // },
+
+        // // And if we need scrollbar
+        // scrollbar: {
+        //   el: '.swiper-scrollbar',
+        // },
+    });
+}
