75fd9853bfaf1afaa1ab59e905619c9209cd52c0
Ashwin Jethawa <Ashwin.Jethawa@teknopoint.local>
2025-03-20 15:50:37 +0530

    Intial Changes

--- blocks/abcabc/abcabc.js
+++ blocks/abcabc/abcabc.js
@@ -1,11 +1,8 @@
 export default function decorate(block) {
-    console.log(block.parentNode.nextSibling.children[0].children);
-    [...block.parentNode.nextSibling.children[0].children].forEach((element, index) => {
-        element.classList.add("row" + (index + 1))
-        console.log(element.classList);
-        [...element.children].forEach((elementsec, jindex) => {
-            elementsec.setAttribute("class","row"+(index+1)+" "+"col"+(jindex+1))
-            console.log(elementsec);
-        })
-    })
+  [...block.parentNode.nextSibling.children[0].children].forEach((element, index) => {
+    element.classList.add(`row${index + 1}`);
+    [...element.children].forEach((elementsec, jindex) => {
+      elementsec.setAttribute('class', `row${index + 1}  col${jindex + 1}`);
+    });
+  });
 }
--- blocks/carusel/carusel.js
+++ blocks/carusel/carusel.js
@@ -1,51 +1,50 @@
-import SwiperCall from "../carusel/swiper-bundle.min.js"
+import SwiperCall from './swiper-bundle.min.js';
+
 export default function decorate(block) {
-    // console.log(block);
-    block.classList.add("swiper")
+  // console.log(block);
+  block.classList.add('swiper');
 
-    const swiperWrapper = document.createElement('div');
-    swiperWrapper.classList.add('swiper-wrapper');
-    Array.from(block.children).forEach((row) => {
-        row.classList.add("swiper-slide")
-        swiperWrapper.appendChild(row)
-    })
-    block.append(swiperWrapper);
+  const swiperWrapper = document.createElement('div');
+  swiperWrapper.classList.add('swiper-wrapper');
+  Array.from(block.children).forEach((row) => {
+    row.classList.add('swiper-slide');
+    swiperWrapper.appendChild(row);
+  });
+  block.append(swiperWrapper);
 
-    const paginationWrapper = document.createElement("div")
-    paginationWrapper.classList.add("swiper-pagination");
-    block.appendChild(paginationWrapper);
+  const paginationWrapper = document.createElement('div');
+  paginationWrapper.classList.add('swiper-pagination');
+  block.appendChild(paginationWrapper);
 
-    const LeftArrow = document.createElement("div");
-    LeftArrow.classList.add("swiper-button-prev");
-    block.appendChild(LeftArrow);
+  const LeftArrow = document.createElement('div');
+  LeftArrow.classList.add('swiper-button-prev');
+  block.appendChild(LeftArrow);
 
-    const RightArrow = document.createElement("div")
-    RightArrow.classList.add("swiper-button-next");
-    block.appendChild(RightArrow);
+  const RightArrow = document.createElement('div');
+  RightArrow.classList.add('swiper-button-next');
+  block.appendChild(RightArrow);
 
+  SwiperCall(block, {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    autoplay: true,
 
-    SwiperCall(block, {
-        // Optional parameters
-        // direction: 'vertical',
-        loop: true,
-        autoplay: true,
+    // If we need pagination
+    pagination: {
+      el: paginationWrapper,
+      clickable: true,
+    },
 
-        // If we need pagination
-        pagination: {
-            el: paginationWrapper,
-            clickable: true,
-        },
+    // // Navigation arrows
+    navigation: {
+      nextEl: RightArrow,
+      prevEl: LeftArrow,
+    },
 
-        // // Navigation arrows
-        navigation: {
-            nextEl: RightArrow,
-            prevEl: LeftArrow,
-        },
-
-        // // And if we need scrollbar
-        // scrollbar: {
-        //   el: '.swiper-scrollbar',
-        // },
-    });
-
+    // // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
 }
